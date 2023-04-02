5620b43aea52ace79ab350f50d31bde6bf884b94
AndrewD <andrewdreste1@gmail.com>
2023-04-02 15:14:02 -0500

    define aces

--- js/app.js
+++ js/app.js
@@ -138,3 +138,28 @@ hitButton.addEventListener("click", () => {
         }
       }
       
+      // Define Ace's value as 1 or 11 depending on hand value
+function getCardValue(card) {
+    switch (card.value) {
+      case 'Ace':
+        return 11;
+      case '2':
+        return 2;
+      case '3':
+        return 3;
+      case '4':
+        return 4;
+      case '5':
+        return 5;
+      case '6':
+        return 6;
+      case '7':
+        return 7;
+      case '8':
+        return 8;
+      case '9':
+        return 9;
+      default:
+        return 10;
+    }
+  }
