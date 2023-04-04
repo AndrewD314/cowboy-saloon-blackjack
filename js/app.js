de00f692ddc6923f978dde95f0cd162d50079124
AndrewD <andrewdreste1@gmail.com>
2023-04-04 10:38:43 -0500

    js mutation

--- js/app.js
+++ js/app.js
@@ -1,147 +1,167 @@
 
-let dealerSum = 0
-let yourSum = 0
+let suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+let deck = new Array()
 
-let dealerAceCount = 0
-let yourAceCount = 0
-
-let hidden
-let deck
-
-let canHit = true //allows the player (you) to draw while yourSum <= 21
-
-window.onload = function() {
-    buildDeck()
-    shuffleDeck()
-    startGame()
-}
-
-function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"]
-    deck = []
-
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + types[i]) //A-C -> K-C, A-D -> K-D
+function createDeck()
+{
+    deck = new Array()
+    for (let i = 0 ; i < values.length; i++)
+    {
+        for(let x = 0; x < suits.length; x++)
+        {
+            let weight = parseInt(values[i])
+            if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                weight = 10
+            if (values[i] == "A")
+                weight = 11
+            let card = { Value: values[i], Suit: suits[x], Weight: weight }
+            deck.push(card)
         }
     }
 }
 
-function shuffleDeck() {
-    for (let i = 0; i < deck.length; i++) {
-        let j = Math.floor(Math.random() * deck.length) // (0-1) * 52 => (0-51.9999)
-        let temp = deck[i]
-        deck[i] = deck[j]
-        deck[j] = temp
+function shuffle()
+{
+    
+    for (var i = 0; i < 1000; i++)
+    {
+        let location1 = Math.floor((Math.random() * deck.length))
+        let location2 = Math.floor((Math.random() * deck.length))
+        let tmp = deck[location1]
+
+        deck[location1] = deck[location2]
+        deck[location2] = tmp
     }
-    console.log(deck)
 }
 
-function startGame() {
-    hidden = deck.pop()
-    dealerSum += getValue(hidden)
-    dealerAceCount += checkAce(hidden)
-    // console.log(hidden);
-    // console.log(dealerSum);
-    while (dealerSum < 17) {
-        //<img src="./cards/4-C.png">
-        let cardImg = document.createElement("img")
-        let card = deck.pop()
-        cardImg.src = "./cards/" + card + ".png"
-        dealerSum += getValue(card)
-        dealerAceCount += checkAce(card)
-        document.getElementById("dealer-cards").append(cardImg)
+let players = new Array()
+function createPlayers(num)
+{
+    players = new Array()
+    for(let i = 1; i <= num; i++)
+    {
+        let hand = new Array()
+        let player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand }
+        players.push(player)
     }
-    console.log(dealerSum)
+}
+function createPlayersUI()
+{
+    document.getElementById('players').innerHTML = ''
+    for(let i = 0; i < players.length; i++)
+    {
+        let div_player = document.createElement('div')
+        let div_playerid = document.createElement('div')
+        let div_hand = document.createElement('div')
+        let div_points = document.createElement('div')
 
-    for (let i = 0; i < 2; i++) {
-        let cardImg = document.createElement("img")
-        let card = deck.pop()
-        cardImg.src = "./cards/" + card + ".png"
-        yourSum += getValue(card)
-        yourAceCount += checkAce(card)
-        document.getElementById("your-cards").append(cardImg)
+        div_points.className = 'points'
+        div_points.id = 'points_' + i
+        div_player.id = 'player_' + i
+        div_player.className = 'player'
+        div_hand.id = 'hand_' + i
+
+        div_playerid.innerHTML = players[i].ID
+        div_player.appendChild(div_playerid)
+        div_player.appendChild(div_hand)
+        div_player.appendChild(div_points)
+        document.getElementById('players').appendChild(div_player)
     }
-
-    console.log(yourSum)
-    document.getElementById("hit").addEventListener("click", hit)
-    document.getElementById("stay").addEventListener("click", stay)
-
 }
 
-function hit() {
-    if (!canHit) {
-        return
+function startblackjack()
+{
+    document.getElementById('btnStart').value = 'Restart'
+    document.getElementById("status").style.display="none"
+   
+    currentPlayer = 0
+    createDeck()
+    shuffle()
+    createPlayers(2)
+    createPlayersUI()
+    dealHands()
+    document.getElementById('player_' + currentPlayer).classList.add('active')
+}
+
+function dealHands()
+{
+  
+    for(let i = 0; i < 2; i++)
+    {
+        for (let x = 0; x < players.length; x++)
+        {
+            let card = deck.pop()
+            players[x].Hand.push(card)
+            renderCard(card, x)
+            updatePoints()
+        }
     }
 
-    let cardImg = document.createElement("img")
+    updateDeck()
+}
+
+function renderCard(card, player)
+{
+    let hand = document.getElementById('hand_' + player)
+    hand.appendChild(getCardUI(card))
+}
+
+function getCardUI(card)
+{
+    let el = document.createElement('div')
+    el.className = 'card'
+    el.innerHTML = card.Suit + ' ' + card.Value
+    return el
+}
+let currentPlayer = 0
+
+function hitMe()
+{
+    
     let card = deck.pop()
-    cardImg.src = "./cards/" + card + ".png"
-    yourSum += getValue(card)
-    yourAceCount += checkAce(card)
-    document.getElementById("your-cards").append(cardImg)
-
-    if (reduceAce(yourSum, yourAceCount) > 21) { //A, J, 8 -> 1 + 10 + 8
-        canHit = false
-    }
-
+    players[currentPlayer].Hand.push(card)
+    renderCard(card, currentPlayer)
+    updatePoints()
+    check()
 }
 
-function stay() {
-    dealerSum = reduceAce(dealerSum, dealerAceCount)
-    yourSum = reduceAce(yourSum, yourAceCount)
-
-    canHit = false
-    document.getElementById("hidden").src = "./cards/" + hidden + ".png"
-
-    let message = ""
-    if (yourSum > 21) {
-        message = "You Lose!"
+function check()
+{
+    if (players[currentPlayer].Points > 21)
+    {
+        document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].ID + ' LOST'
     }
-    else if (dealerSum > 21) {
-        message = "You win!"
-    }
-    //both you and dealer <= 21
-    else if (yourSum == dealerSum) {
-        message = "Tie!"
-    }
-    else if (yourSum > dealerSum) {
-        message = "You Win!"
-    }
-    else if (yourSum < dealerSum) {
-        message = "You Lose!"
-    }
-
-    document.getElementById("dealer-sum").innerText = dealerSum
-    document.getElementById("your-sum").innerText = yourSum
-    document.getElementById("results").innerText = message
 }
 
-function getValue(card) {
-    let data = card.split("-") // "4-C" -> ["4", "C"]
-    let value = data[0]
+function stay()
+{
+    
+    if (currentPlayer != players.length-1) {
+        document.getElementById('player_' + currentPlayer).classList.remove('active')
+        currentPlayer += 1
+        document.getElementById('player_' + currentPlayer).classList.add('active')
+    }
 
-    if (isNaN(value)) { //A J Q K
-        if (value == "A") {
-            return 11
+    else {
+        end()
+    }
+}
+
+function end()
+{
+    let winner = -1
+    let score = 0
+
+    for(var i = 0; i < players.length; i++)
+    {
+        if (players[i].Points > score && players[i].Points < 22)
+        {
+            winner = i
         }
-        return 10
-    }
-    return parseInt(value)
-}
 
-function checkAce(card) {
-    if (card[0] == "A") {
-        return 1
+        score = players[i].Points
     }
-    return 0
-}
 
-function reduceAce(playerSum, playerAceCount) {
-    while (playerSum > 21 && playerAceCount > 0) {
-        playerSum -= 10
-        playerAceCount -= 1
-    }
-    return playerSum
+    document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].ID
 }
